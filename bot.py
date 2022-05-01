fcbd084487a180e67ef0d6ea47b5013572241b1d
Joshiy13 <79088559+Joshiy13@users.noreply.github.com>
2022-05-01 16:49:05 +0200

    added commands

--- bot.py
+++ bot.py
@@ -2,7 +2,7 @@ from tokenize import Token
 from turtle import color
 from unicodedata import name
 import discord
-from discord import guild, embeds
+from discord import guild, embeds, commands
 import asyncio
 import os
 
@@ -18,6 +18,8 @@ help.add_field(name="!avatar", value="Shows your Avatar", inline=False)
 help.add_field(name="!say", value="Sends a message trough the Bot", inline=False)
 help.add_field(name="!code", value="Shows the Source Code to this Bot", inline=False)
 help.add_field(name="!telldev", value="Pings the Dev of this Bot", inline=False)
+help.add_field(name="!ban", value="Bans a User", inline=False)
+help.add_field(name="!kick", value="Kicks a User", inline=False)
 
 code = discord.Embed(title="Source Code:", description="https://github.com/Joshiy13/Ryllz", color=800080)
 
@@ -36,6 +38,41 @@ async def status_task():
         await client.change_presence(activity=discord.Game("https://discord.gg/PPjDtEYRnn"), status=discord.Status.online)
         await asyncio.sleep(10)
 
+@client.event
+async def on_command_error(ctx, error):
+    if isinstance(error, commands.MissingRequiredArgument):
+        await ctx.send('Please pass in all requirements :rolling_eyes:.')
+    if isinstance(error, commands.MissingPermissions):
+        await ctx.send("You dont have all the requirements :angry:")
+
+#The below code bans player.
+@client.command()
+@commands.has_permissions(ban_members = True)
+async def ban(ctx, member : discord.Member, *, reason = None):
+    await member.ban(reason = reason)
+
+#The below code unbans player.
+@client.command()
+@commands.has_permissions(administrator = True)
+async def unban(ctx, *, member):
+    banned_users = await ctx.guild.bans()
+    member_name, member_discriminator = member.split("#")
+
+    for ban_entry in banned_users:
+        user = ban_entry.user
+
+        if (user.name, user.discriminator) == (member_name, member_discriminator):
+            await ctx.guild.unban(user)
+            await ctx.send(f'Unbanned {user.mention}')
+            return
+
+
+#The below code kicks player.
+@client.command()
+@commands.has_permissions(kick_members = True)
+async def kick(ctx, member : discord.Member, *, reason = None):
+    await member.kick(reason = reason)
+    
 
 @client.event
 async def on_message(message):
@@ -53,6 +90,7 @@ async def on_message(message):
         await message.channel.send(f"<@{dev}>" + " " + message.content[9:])
     if message.content.startswith("!code"):
         await message.channel.send(embed=code)
+
 
 
 client.run(token)
